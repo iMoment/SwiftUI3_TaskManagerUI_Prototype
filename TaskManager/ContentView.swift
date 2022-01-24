e3fc00dffc6a49deab2880d091a8fda601ec11fb
Stanley Pan <imoment.developer@gmail.com>
2022-01-24 15:27:40 -0500

    Initial Commit

---
+++ TaskManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManager
+//
+//  Created by Stanley Pan on 2022/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Stanley Pan on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
